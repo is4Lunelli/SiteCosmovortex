dc07aa86e91c818e3358753c4d7e219f69e39039
Lunelli <isa.lunelli23@gmail.com>
2024-09-15 13:08:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function toggle(el) {
     const element = document.getElementById(el);
-    element.style.display = element.style.display === 'none' ? 'block' : 'none';
-  }
+    if (element.style.display === 'block') {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'block';
+    }
+}
