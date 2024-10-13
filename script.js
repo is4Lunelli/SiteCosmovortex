ebebfcb95f2adff7d7bc3171e19dd52fe15a2a01
Lunelli <isa.lunelli23@gmail.com>
2024-10-12 22:17:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function toggle(el) {
 function abrirFormulario() {
   window.open('https://docs.google.com/forms/d/e/1FAIpQLSc_Un9gOOgdoD7qdfRLD6d-9vAkmYWXOJDpi2ZxRIf_m8oqEw/viewform?usp=sf_link', '_blank');
 }
+
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "./img/img/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "./img/img/close_white_36dp.svg";
+  }
+}
